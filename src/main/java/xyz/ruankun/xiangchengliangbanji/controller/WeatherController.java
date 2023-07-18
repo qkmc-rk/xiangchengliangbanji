5f8f0ab9845dfc63ed1b73b49c2e5339f431ae44
qkmc <qkmc@outlook.com>
2023-07-18 11:34:31 +0800

    增加新的天气接口

--- src/main/java/xyz/ruankun/xiangchengliangbanji/controller/WeatherController.java
+++ src/main/java/xyz/ruankun/xiangchengliangbanji/controller/WeatherController.java
@@ -1,5 +1,6 @@
 package xyz.ruankun.xiangchengliangbanji.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.BufferedReader;
@@ -7,17 +8,38 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.zip.GZIPInputStream;
 
 @RestController
 @RequestMapping("/api/weather")
 @CrossOrigin
 public class WeatherController {
 
+    @Value("${hefeng.key}")
+    private String key;
+
     @GetMapping
     public String  getCurrentWeather(@RequestParam("lon") String lon, @RequestParam("lat")String lat)  throws IOException {
-
         URL url = new URL("http://api.caiyunapp.com/v2.6/NMFkcX12TkkmxzQq/" + lat + "," + lon +"/weather?alert=true&dailysteps=1&hourlysteps=24");
-        System.out.println(url);
+        return connect(url, false);
+    }
+
+    @GetMapping("/hefeng")
+    public String  getCurrentWeatherByHefeng(@RequestParam("location") String location, @RequestParam(required = false, value = "key") String key)  throws IOException {
+        if(key!= null && !key.equals("")){
+            this.key = key;
+        }
+        URL url = new URL("https://devapi.qweather.com/v7/weather/now?key=" + this.key + "&location=" + location);
+        return connect(url, true);
+    }
+
+    /**
+     * 抽取一下私有方法
+     * @param url
+     * @return
+     * @throws IOException
+     */
+    private String connect(URL url, Boolean gzip) throws IOException{
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("GET");
         connection.setRequestProperty("Content-Type", "application/json");
@@ -26,7 +48,12 @@ public class WeatherController {
 
         int responseCode = connection.getResponseCode();
         if (responseCode == HttpURLConnection.HTTP_OK) {
-            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            BufferedReader in;
+            if (gzip){
+                in = new BufferedReader(new InputStreamReader(new GZIPInputStream(connection.getInputStream()), "UTF-8"));
+            }else {
+                in = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
+            }
             String inputLine;
             StringBuilder response = new StringBuilder();
 
@@ -34,7 +61,6 @@ public class WeatherController {
                 response.append(inputLine);
             }
             in.close();
-
             return response.toString();
         } else {
             return "Error: " + responseCode;
